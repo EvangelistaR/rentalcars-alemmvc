12ced22418932befd1ef5e4369b99902201200c3
Rafael EvanS <rafael.evans@live.com>
2019-11-28 19:36:12 -0300

    Workshop design pattern, day 3

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,7 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: %i[confirm]
+  before_action :authorize_user!, only: %i[confirm]
+
   def index
     @rentals = Rental.where(subsidiary: current_subsidiary)
   end
@@ -10,18 +13,32 @@ class RentalsController < ApplicationController
   end
 
   def create
-    @rental = Rental.new(rental_params)
-    subsidiary = current_subsidiary
-    category = Category.find(params['rental']['category_id'])
-    @rental.subsidiary = subsidiary
-    @rental.status = :scheduled
-    @rental.price_projection = @rental.calculate_price_projection
-    if @rental.save
-      redirect_to rental_path(@rental.id)
+    # @rental = Rental.new(rental_params)
+    @rental = RentalBuilder.new(rental_params, current_subsidiary).build
+
+    # Tudo isso está dentro do Builder de Rental
+    # subsidiary = current_subsidiary
+    # category = Category.find(params['rental']['category_id'])
+    # @rental.subsidiary = subsidiary
+    # @rental.status = :scheduled
+    # @rental.price_projection = @rental.calculate_price_projection
+
+    return redirect_to rental_path(@rental.id) if @rental.save
+    @clients = Client.all
+    @categories = Category.all
+    render :new
+  end
+
+  def confirm
+    if RentalConfirmer.new(@rental,params[car_id], params[:addon_ids]).confirm
+      @car = @rental.car
+
+      render :confirm
     else
-      @clients = Client.all
-      @categories = Category.all
-      render :new
+      flash[:danger] = "Carro deve ser selecionado"
+      @cars = @rental.available_cars
+      @addons = Addon.joins(:addon_items).where(addon_items: { status: :available  }).group(:id)
+      render :review
     end
   end
 
@@ -29,10 +46,42 @@ class RentalsController < ApplicationController
     @rental = RentalPresenter.new(Rental.find(params[:id]))
   end
 
+  def search
+    @rental = Rental.find_by(reservation_code: params[:q])
+    return redirect_to review_rental_path(@rental) if @rental
+  end
+
+  def review
+    @rental = Rental.find(params[:id])
+    @rental.in_review!
+    @cars = @rental.available_cars
+    @addons = Addon.joins(:addon_items).where(addon_items: { status: :available  }).group(:id)
+  end
+
+  def start
+    @rental = Rental.find(params[:id])
+    @rental.ongoing!
+    redirect_to @rental
+  end
   private
 
   def rental_params
     params.require(:rental).permit(:category_id, :client_id, :start_date,
-                                   :end_date)
+                                   :end_date,
+                                   rental_items_attributes: [:car_id])
+  end
+
+  def set_rental
+    @rental.fid(params[:id])
+  end
+
+  def authorize_user!
+    return if authorized?
+
+    return redirect_to @rental
+  end
+
+  def authorized?
+    RentalAuthorized.new(@rental, current_user).authorized?
   end
 end
--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -7,4 +7,10 @@ class CarModel < ApplicationRecord
   validates :name, presence: true
   validates :year, presence: true
   validates :car_options, presence: true
+
+  def daily_rate
+    category.daily_rate +
+    category.third_party_insurance +
+    category.car_insurance
+  end
 end
